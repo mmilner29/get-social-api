d4f41cdc045b5955c3b1f93bfa1d52ae65b154a4
mmilner29 <madisonm4514@gmail.com>
2022-06-04 16:21:14 -0700

    Reaction.js model

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const ReactionSchema = new Schema ({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    }
+},
+{
+    toJSON: {
+        getters: true,
+    }
+}
+)
+
+const Reaction = model('Reaction', ReactionSchema);
+
+module.exports = Reaction;
