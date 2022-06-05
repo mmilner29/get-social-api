ad9002d43ce530e5a3118943cc7c263042deb57a
mmilner29 <madisonm4514@gmail.com>
2022-06-04 17:20:41 -0700

    user routes & controllers

---
+++ routes/api/users-routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controller');
